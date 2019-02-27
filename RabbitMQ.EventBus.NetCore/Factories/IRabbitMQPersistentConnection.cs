b132cf475b8d009a1d2912ec5e3a36fa9879538c
Renato burton <reantorganics@outlook.com>
2019-02-27 15:01:31 -0300

    Fix

--- RabbitMQ.EventBus.NetCore/Factories/IRabbitMQPersistentConnection.cs
+++ RabbitMQ.EventBus.NetCore/Factories/IRabbitMQPersistentConnection.cs
@@ -8,28 +8,10 @@ namespace RabbitMQ.EventBus.NetCore.Factories
 {
     public interface IRabbitMQPersistentConnection : IDisposable
     {
-        /// <summary>
-        /// 
-        /// </summary>
         RabbitMQEventBusConnectionConfiguration Configuration { get; }
-        /// <summary>
-        /// 连接点
-        /// </summary>
         string Endpoint { get; }
-        /// <summary>
-        /// 是否打开链接
-        /// </summary>
         bool IsConnected { get; }
-        /// <summary>
-        /// 尝试链接
-        /// </summary>
-        /// <returns></returns>
         bool TryConnect();
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
         IModel CreateModel();
-
     }
 }
